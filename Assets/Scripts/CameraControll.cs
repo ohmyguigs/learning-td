937ad289309bca6b85475fb49f06eb8465f6d669
guigs <ohmyguigs@gmail.com>
2023-05-03 22:19:23 -0300

    starting shop (wip)

--- Assets/Scripts/CameraControll.cs
+++ Assets/Scripts/CameraControll.cs
@@ -1,62 +1,54 @@
 using UnityEngine;
-using Cinemachine;
 
-public class CameraControll : MonoBehaviour {
-    [SerializeField]
-    private float panSpeed = 20f;
-    [SerializeField]
-    private float zoomSpeed = 15f;
-    [SerializeField]
-    private float zoomInMax = 40f;
-    [SerializeField]
-    private float zoomOutMax = 90f;
-    private CinemachineInputProvider inputProvider;
-    private CinemachineVirtualCamera vcam;
-    private Transform cameraTransform;
+public class CameraController : MonoBehaviour {
+  public float panSpeed = 30f;
+  public float panBorderThickness = 10f;
+  public float scrollSpeed = 5f;
+  public float minY = 10f;
+  public float maxY = 80f;
+  public float minZoom = 1f;
+  public float maxZoom = 40f;
+  public float zoomDamp = 1f;
+  public float zoomStep = 13f;
 
-    private void Awake() {
-        inputProvider = GetComponent<CinemachineInputProvider>();
-        vcam = GetComponent<CinemachineVirtualCamera>();
-        cameraTransform = vcam.VirtualCameraGameObject.transform;
+  private Camera mainCam;
+
+  void Awake() {
+    mainCam = GetComponent<Camera>();
+  }
+
+  void Update() {
+
+    /*
+    if (GameManager.GameIsOver) {
+        this.enabled = false;
+        return;
+    }
+        */
+
+    if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness) {
+      transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+    }
+    if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness) {
+      transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+    }
+    if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness) {
+      transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+    }
+    if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness) {
+      transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
     }
 
-    void Start() {
-        
+    float scroll = Input.GetAxis("Mouse ScrollWheel");
+    if (scroll != 0f) {
+      Debug.Log("scroll?? " + scroll + " camera pos: " + Camera.main.fieldOfView);
+
+      float targetView = Camera.main.fieldOfView;
+      targetView -= scroll * 1000 * zoomStep;
+
+      targetView = Mathf.Clamp(targetView, minZoom, maxZoom);
+      mainCam.fieldOfView = Mathf.SmoothDamp(mainCam.fieldOfView, targetView, ref scrollSpeed, zoomDamp);
     }
 
-    void Update() {
-        float x = inputProvider.GetAxisValue(0);
-        float y = inputProvider.GetAxisValue(1);
-
-        if (x != 0 || y != 0) {
-            PanScreen(x, y);
-        }
-    }
-
-    public Vector2 PanDirection(float x, float y) {
-        Vector2 direction = Vector2.zero;
-        if (y >= Screen.height * .95f) {
-            direction.y += 1;
-        } else if (y <= Screen.height * .05f) {
-            direction.y -= 1;
-        }
-
-        if (x >= Screen.width * .95f) {
-            direction.x += 1;
-        } else if (x <= Screen.width * .05f) {
-            direction.x -= 1;
-        }
-        return direction;
-    }
-
-    public void PanScreen(float x, float y) {
-        Vector2 direction = PanDirection(x, y);
-        cameraTransform.position = Vector3.Lerp(cameraTransform.position, cameraTransform.position + (Vector3)direction, Time.deltaTime * panSpeed);
-    }
-
-    public void ZoomScreen(float increment) {
-        float fov = vcam.m_Lens.FieldOfView;
-        float target = Mathf.Clamp(fov + increment, zoomInMax, zoomOutMax);
-        vcam.m_Lens.FieldOfView = Mathf.Lerp(fov, target, Time.deltaTime * zoomSpeed);
-    }
+}
 }
